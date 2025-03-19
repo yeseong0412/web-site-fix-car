745da08ce800c9585f486fc5a12e6612043c39fb
yeseong0412 <yeseong0412@dgsw.hs.kr>
2025-03-19 16:23:29 +0900

    Add Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Services from './components/Services';
 import Reviews from './components/Reviews';
 import Footer from './components/Footer';
 import BookingAndContact from './components/BookingAndContact.tsx';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
     return (
         <div className="min-h-screen">
+            <Analytics />
             <Helmet>
                 <title>강남 자동차 에어컨 수리 | 강남 카 에어컨</title>
                 <meta name="description" content="강남에서 전문적인 자동차 에어컨 수리 서비스를 제공합니다. 고객 맞춤형 서비스와 빠른 수리." />
